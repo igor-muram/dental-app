92afb1045c04e47d2504b298fb80cfd64c18cf3a
IgorMuramshchikov <igor.muram@gmail.com>
2021-10-19 12:25:11 +0700

    Add phones in Data

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,6 +15,7 @@ const DATA = [
         diagnosis: 'Пульпит, удаление зуба',
         active: true,
         user: {
+          phone: '+7 (999) 490 09 20',
           fullname: 'Максимова Софья',
           avatar:
             'https://sun9-31.userapi.com/impg/0C3JI3rZQ7sPIBjOoVKvPaq2wlne2LXW3BoybA/7N25TRH5oXQ.jpg?size=703x1080&quality=96&sign=73ed7a75ac15dfc81c87d73c881794f6&type=album',
@@ -24,6 +25,7 @@ const DATA = [
         time: '13:40',
         diagnosis: 'Пульпит',
         user: {
+          phone: '+7 (923) 148 44 74',
           fullname: 'Иванов Максим',
         },
       },
@@ -31,6 +33,7 @@ const DATA = [
         time: '15:30',
         diagnosis: 'Пародонтит',
         user: {
+          phone: '+7 (999) 115 45 85',
           fullname: 'Функ Софья',
           avatar:
             'https://sun9-26.userapi.com/impf/c846220/v846220730/b5a54/De-pCwtLs8k.jpg?size=2047x2047&quality=96&sign=ddc72e2cb4d21886df608557859764e3&type=album',
@@ -40,6 +43,7 @@ const DATA = [
         time: '16:00',
         diagnosis: 'Чистка зубов',
         user: {
+          phone: '+7 (913) 154 44 86',
           fullname: 'Январев Никита',
           avatar:
             'https://sun9-87.userapi.com/impf/c830509/v830509615/1ac85d/aN2pxMtbWUc.jpg?size=720x960&quality=96&sign=6d93bf11ad3440a3bb78b26bd8edf1a1&type=album',
@@ -49,6 +53,7 @@ const DATA = [
         time: '18:30',
         diagnosis: 'Кариес',
         user: {
+          phone: '+7 (925) 542 11 45',
           fullname: 'Вальцев Кирилл',
         },
       },
@@ -56,6 +61,7 @@ const DATA = [
         time: '19:30',
         diagnosis: 'Чистка зубов',
         user: {
+          phone: '+7 (999) 486 48 22',
           fullname: 'Бергер Екатерина',
           avatar:
             'https://sun9-1.userapi.com/impg/EssbqkrZfy7uaexqiBpCv36LQIHPOVo_ph2PlQ/YNcEdgaCPaA.jpg?size=607x1080&quality=96&sign=d78b71e4162113c94081f5049a59641c&type=album',
@@ -70,6 +76,7 @@ const DATA = [
         time: '11:00',
         diagnosis: 'Лечение зуба',
         user: {
+          phone: '+7 (913) 648 41 12',
           fullname: 'Макаров Антон',
           avatar: '',
         },
@@ -78,6 +85,7 @@ const DATA = [
         time: '12:30',
         diagnosis: 'Пломба',
         user: {
+          phone: '+7 (925) 455 45 10',
           fullname: 'Функ Софья',
           avatar:
             'https://sun9-26.userapi.com/impf/c846220/v846220730/b5a54/De-pCwtLs8k.jpg?size=2047x2047&quality=96&sign=ddc72e2cb4d21886df608557859764e3&type=album',
@@ -87,6 +95,7 @@ const DATA = [
         time: '15:00',
         diagnosis: 'Пульпит',
         user: {
+          phone: '+7 (996) 489 11 12',
           fullname: 'Полынцева Евгения',
           avatar:
             'https://sun9-19.userapi.com/impg/LJthW9uPjKW4HRzBH2btdBmdOqU2JKF3qJ8YnQ/XNBDMntKIpk.jpg?size=812x1080&quality=96&sign=eea7428cb73f956cdc5a38b6a90e5fd8&type=album',
@@ -96,6 +105,7 @@ const DATA = [
         time: '17:30',
         diagnosis: 'Пародонтит',
         user: {
+          phone: '+7 (925) 936 78 41',
           fullname: 'Бергер Екатерина',
           avatar:
             'https://sun9-1.userapi.com/impg/EssbqkrZfy7uaexqiBpCv36LQIHPOVo_ph2PlQ/YNcEdgaCPaA.jpg?size=607x1080&quality=96&sign=d78b71e4162113c94081f5049a59641c&type=album',
@@ -111,7 +121,7 @@ export default function HomeScreen({ navigation }) {
       <SectionList
         sections={DATA}
         keyExtractor={(name, index) => name + index}
-        renderItem={({ item }) => <Appointment navigate={navigation.navigate} {...item} />}
+        renderItem={({ item }) => <Appointment navigate={navigation.navigate} item={item} />}
         renderSectionHeader={({ section: { title } }) => <SectionTitle>{title}</SectionTitle>}
       />
       <PlusButton>
